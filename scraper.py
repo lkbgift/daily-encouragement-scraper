9242ee961d22a9ea9f4756549b1e329eced084d1
lkbgift <lkbgift@gmail.com>
2013-05-28 17:10:57 -0700

    works when going backwards

--- scraper.py
+++ scraper.py
@@ -21,47 +21,28 @@ def scrape_table(root):
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
             scraperwiki.sqlite.save(["Quote"], record)
-
-# Doesnt do anything        
-def scrape_metal():
-    html = scraperwiki.scrape(url)
-    print html
-    root = lxml.html.fromstring(html)
-    scrape_table(root)
-    dated = '?m=' + str(month) + '&d=' + str(day) 
-    recent_url = urlparse.urljoin(base_url, dated)
-    scrape_and_get_next_date(recent_url)
-
-# Doesnt do anything    
-def day_progression():
-    for day in range (1,31):
-        print "day " + str(day)
-        scrape_metal
-
-# Doesnt do anything    
-def scrape_and_get_next_date(url):
-    for month in range (1,13):
-        day_progression()
-        print "month " + str(month)
-
+        
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
 def scrape_and_look_for_next_link(url):
     html = scraperwiki.scrape(url)
     print html
     root = lxml.html.fromstring(html)
-    scrape_table(root)
-    next_link = root.cssselect("a.next")
-    print next_link
-    if next_link:
-        next_url = urlparse.urljoin(base_url, next_link[0].attrib.get('href'))
-        print next_url
-        scrape_and_look_for_next_link(next_url)
+    rows = root.cssselect("div#body_right")  # selects all <tr> blocks within <table class="data">
+    for row in rows:
+        # Set up our data record - we'll need it later
+        scrape_table(root)
+        next_link = row.cssselect("a")
+        print next_link
+        if next_link:
+            next_url = urlparse.urljoin(base_url, next_link[1].attrib.get('href'))
+            print next_url
+            scrape_and_look_for_next_link(next_url)
 
 # ---------------------------------------------------------------------------
 # START HERE: define your starting URL - then 
 # call a function to scrape the first page in the series.
 # ---------------------------------------------------------------------------
-base_url = 'http://www.sgi-usa.org/encouragement/index.php'
-starting_url = urlparse.urljoin(base_url, '?m=1&d=1')
+base_url = 'http://www.sgi-usa.org/encouragement/index.php/'
+starting_url = urlparse.urljoin(base_url, '?')
 scrape_and_look_for_next_link(starting_url)
